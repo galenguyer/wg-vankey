b1ecea769b53e06fab994e278f7522b733cad073
Galen Guyer <galen@galenguyer.com>
2020-08-02 03:39:18 -0400

    Simple code to create new key pair

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate base64;
+extern crate ed25519_dalek;
+extern crate rand;
+
+use ed25519_dalek::{Keypair, PublicKey};
+use rand::rngs::OsRng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut csprng = OsRng {};
+    let keypair: Keypair = Keypair::generate(&mut csprng);
+    let public_key: PublicKey = keypair.public;
+    println!("{}", base64::encode(public_key));
 }
