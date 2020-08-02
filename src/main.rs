078d047731ae18a288c17c41a8610a7622a0fe82
Galen Guyer <galen@galenguyer.com>
2020-08-02 03:57:04 -0400

    Generate a lot of key pairs, with Options

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ extern crate base64;
 extern crate ed25519_dalek;
 extern crate rand;
 
-use ed25519_dalek::{Keypair, PublicKey};
+use ed25519_dalek::Keypair;
 use rand::rngs::OsRng;
 
 fn main() {
+    while true {
+        match try_pair() {
+             Some((pubkey, privkey)) => println!("public: {} private: {}", pubkey, privkey),
+             None => {},
+	}
+    }
+}
+
+fn try_pair() -> Option<(String, String)> {
     let mut csprng = OsRng {};
     let keypair: Keypair = Keypair::generate(&mut csprng);
-    let public_key: PublicKey = keypair.public;
-    println!("{}", base64::encode(public_key));
+    let public_key = base64::encode(keypair.public);
+
+    return Some((public_key, base64::encode(keypair.secret)));
 }
