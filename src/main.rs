ae369750e055eef9a4a0e48aeff7823b2bb9f806
Galen Guyer <galen@galenguyer.com>
2020-08-02 08:48:15 -0700

    Add time for one attempt and est attempts per key

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate base64;
 extern crate ed25519_dalek;
 extern crate rand;
 
+use std::time::{Duration, SystemTime};
 use clap::{App, Arg};
 use ed25519_dalek::Keypair;
 use rand::rngs::OsRng;
@@ -19,8 +20,19 @@ fn main() {
         )
         .get_matches();
 
+    let prefix: &str = matches.value_of("PREFIX").unwrap();
+    let time_for_one: u128 = time_one().as_nanos();
+    println!("time for one attempt: {}", time_for_one);
+    
+    // TODO: Do this with exponents not multiplication loops (maybe)
+    let mut est_attempts_per_key: u64 = 1;
+    prefix.chars().for_each(|_| {
+       est_attempts_per_key *= 64; 
+    });
+    println!("estimated attempts per key: {}", est_attempts_per_key);
+
     loop {
-        if let Some((pubkey, privkey)) = try_pair(matches.value_of("PREFIX").unwrap()) {
+        if let Some((pubkey, privkey)) = try_pair(prefix) {
             println!("public: {} private: {}", pubkey, privkey)
         }
     }
@@ -35,4 +47,14 @@ fn try_pair(prefix: &str) -> Option<(String, String)> {
     } else {
         None
     }
+}
+
+fn time_one() -> Duration {
+    let prefix: &str = "test";
+    let iterations = 1000;
+    let start_time = SystemTime::now();
+    (0..iterations).for_each(|_| {
+        try_pair(prefix);
+    });
+    start_time.elapsed().unwrap().checked_div(iterations).unwrap()
 }
