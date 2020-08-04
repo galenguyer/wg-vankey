5966cdda8eb46c401f1b9a7c34854f6377ed9e33
Galen Guyer <galen@galenguyer.com>
2020-08-04 17:53:25 -0400

    Use Duration instead of nanoseconds for time formatting

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use clap::{App, Arg};
 use ed25519_dalek::Keypair;
 use rand::rngs::OsRng;
 use std::thread;
-use std::time::{Duration, SystemTime};
+use std::time::{Duration, Instant};
 use threadpool::ThreadPool;
 
 fn main() {
     let matches = App::new("wg-vankey")
-        .version("1.0.3")
+        .version("1.1.0")
         .author("Galen Guyer <galen@galenguyer.com>")
         .about("generate vanity wireguard public keys")
         .arg(
@@ -31,7 +31,7 @@ fn main() {
     // to consume it
     let prefix: &'static str = string_to_static_str(matches.value_of("PREFIX").unwrap().to_owned());
     let ignore_case: bool = matches.is_present("ignore-case");
-    // set the core count to either the count given or the number of available cores 
+    // set the core count to either the count given or the number of available cores
     let core_count: usize = match matches.value_of("core-count") {
         Some(val) => usize::from_str_radix(val, 10).unwrap().min(num_cpus::get()),
         None => num_cpus::get(),
@@ -39,15 +39,15 @@ fn main() {
     println!("{} cores available, using {}", num_cpus::get(), core_count);
 
     // estimate how long a single key generation takes for time estimation later
-    let time_for_one: u128 = time_one().as_nanos();
+    let time_for_one: Duration = time_one();
     println!(
-        "time for one attempt: {} ({} keys/second)",
-        format_ns(time_for_one as u64),
-        (1000000000 * core_count / (time_for_one as usize))
+        "time for one attempt: {:?} ({} keys/second)",
+        time_for_one,
+        ((core_count as f64) / time_for_one.as_secs_f64()).round()
     );
 
-    // estimate how many attempts each key will take 
-    let mut est_attempts_per_key: u64 = 1;
+    // estimate how many attempts each key will take
+    let mut est_attempts_per_key: u32 = 1;
     prefix.chars().for_each(|c| {
         // each additional character given increases the needed guesses by 64-fold
         est_attempts_per_key *= 64;
@@ -61,8 +61,12 @@ fn main() {
     // estimate time per key using the time for a single key, the number of cores
     // to use, and how many attempts each key is estimated to take
     println!(
-        "estimated time per key: {}",
-        format_ns((time_for_one / (core_count as u128)) as u64 * est_attempts_per_key)
+        "estimated time per key: {:?}",
+        time_for_one
+            .checked_mul(est_attempts_per_key)
+            .unwrap()
+            .checked_div(core_count as u32)
+            .unwrap()
     );
     println!("press ctrl+c to cancel at any time");
 
@@ -99,35 +103,15 @@ fn try_pair(prefix: &'static str, ignore_case: bool) -> Option<(String, String)>
     }
 }
 
-// run 10,000 iterations of key generation to get a good average of how long each key takes to generate
+// run 1,000 iterations of key generation to get a good average of how long each key takes to generate
 fn time_one() -> Duration {
     let prefix = string_to_static_str(String::from("test"));
-    let iterations = 10000;
-    let start_time = SystemTime::now();
+    let iterations = 1000;
+    let start_time = Instant::now();
     (0..iterations).for_each(|_| {
         try_pair(prefix, false);
     });
-    start_time
-        .elapsed()
-        .unwrap()
-        .checked_div(iterations)
-        .unwrap()
-}
-
-// TODO: cursed_code
-fn format_ns(nanos: u64) -> String {
-    if nanos < 1000 {
-        return format!("{}ns", nanos);
-    } else if nanos < (1000 * 1000) {
-        return format!("{}us", (nanos / 1000));
-    } else if nanos < (1000 * 1000 * 1000) {
-        return format!("{}ms", (nanos / (1000 * 1000)));
-    } else if nanos < (60 * 1000 * 1000 * 1000) {
-        return format!("{}s", (nanos / (1000 * 1000 * 1000)));
-    } else if nanos < (60 * 60 * 1000 * 1000 * 1000) {
-        return format!("{}m", (nanos / (60 * 1000 * 1000 * 1000)));
-    }
-    return format!("{}h", (nanos / (60 * 60 * 1000 * 1000 * 1000)));
+    start_time.elapsed().checked_div(iterations).unwrap()
 }
 
 // needed for ensuring the prefix has a long enough lifetime
